f9f356d36bd252c71ee3ed2d0585cc372f2baf5e
sarat <sarat.sithar@gmail.com>
2019-09-17 20:23:10 +0530

    saved

--- Codeforces/ProblemSet/1037D.cpp
+++ Codeforces/ProblemSet/1037D.cpp
@@ -2,24 +2,35 @@
 #include<vector>
 #include<queue>
 #include<algorithm>
+#include<set>
+
 
 using namespace std;
 
 
 vector<int> G[234567];
 
-int d[234567];
+int a[234567];
+
+int order[234567];
+
+bool visited[234567];
 
 int n;
 
-void bfs(int u){
-	d[u] = 0;
+bool comp(int &x, int &y){
+	return a[x] < a[y];
+}
+
+void bfs(int *a){
 	queue<int> q;
-	q.push(u);
+	q.push(0);
+	int j = 0;
 	while(!q.empty()){
 		int x = q.front();
 		q.pop();
-		for(int i=0; i < G[x].size(); i++) if(d[G[x][i]] == -1) d[G[x][i]] = d[x] + 1,q.push(G[x][i]);
+		order[j++] = x;
+		for(int i=0; i < G[x].size(); i++) if(!visited[G[x][i]]) q.push(G[x][i]);
 	}
 }	
 
@@ -38,24 +49,10 @@ int main(){
 		G[x].push_back(y);
 		G[y].push_back(x);
 	}
-	int a[234567];
 	int x;
-	for(int i =0; i < n; i++) cin >> x, x--,a[i]=x,d[i]=-1;
-	bfs(a[0]);
-	int d2[234567];
-	for(int i=0; i < n; i++) d2[i] = d[i];
-	sort(d2,d2+n);
-	int sum1,sum2;
-	sum1=sum2=0;
-	for(int i=0; i < n; i++){
-		sum1 += d2[i];
-		sum2 += d[a[i]];
-		if(sum1 != sum2){
-			puts("NO");
-			return 0;
-		}
-	}
-	puts("YES");
+	for(int i =0; i < n; i++) cin >> x, x--,a[x]=i,visited[x]=false;
+	for(int i=0; i < n; i++) sort(G[i].begin(),G[i].end(),comp);
+	bfs(a);
 	return 0;
 }	
 	
